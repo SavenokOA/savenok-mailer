9dcd4eb4d5b30d656a2218694adabb4101510f71
SavenokOA <oleg2455553535@gmail.com>
2021-08-04 04:25:05 +0300

    asfasf

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?php
-require_once __DIR__.'/vendor/autoload.php';
 
 use App\Messenger\MailMessenger;
+
+require_once __DIR__.'/vendor/autoload.php';
+
+
 $user = ['oleg2455553535@gmail.com', 'oleg2455553535@gmail.com'];
 $params = require_once('params.php');
 
